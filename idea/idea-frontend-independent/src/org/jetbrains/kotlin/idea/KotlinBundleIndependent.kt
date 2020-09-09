7622001b9b6d8b64bbddde66fb7627a2c506aa24
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2020-09-09 18:25:14 +0300

    XXX

--- idea/idea-frontend-independent/src/org/jetbrains/kotlin/idea/KotlinBundleIndependent.kt
+++ idea/idea-frontend-independent/src/org/jetbrains/kotlin/idea/KotlinBundleIndependent.kt
@@ -4,17 +4,14 @@
  */
 package org.jetbrains.kotlin.idea
 
-import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
 @NonNls
 private const val BUNDLE = "messages.KotlinBundleIndependent"
 
-object KotlinBundleIndependent : DynamicBundle(BUNDLE) {
-
-    private fun String.withHtml(): String = "<html>$this</html>"
-
+//TODO Copy-pasted from KotlinBundle.kt
+object KotlinBundleIndependent : AbstractKotlinBundleIndependent(BUNDLE) {
     @JvmStatic
     fun message(@NonNls @PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String = getMessage(key, *params)
 
